afb76bc2bcb170053feaf259b7156b5229253fdc
Terminator8007 <erwann_n@icloud.com>
2024-10-09 13:16:08 -0400

    Parallaxe : done and functional

---
+++ States/player_idle.gd
@@ -0,0 +1,24 @@
+extends BaseState
+class_name PlayerIdle
+
+@export var player : Player
+var anim_player : AnimationPlayer
+
+func manage_input() -> void:	
+	var dir : Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
+	
+	if (dir.length() > 0):
+		Transitioned.emit(self, "Walk")
+
+func enter():
+	anim_player = player.get_animation_player()	
+	
+func update(delta: float) -> void:
+	if not anim_player :
+		anim_player = player.get_animation_player()
+	manage_input()
+	
+func physics_update(delta: float) -> void:
+	if not anim_player : return
+		
+	anim_player.play("idle")
